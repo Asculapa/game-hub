2e8c9454b31a8d62cb99be84ef47c7cdbbc3ce4c
Olexandr <nadtochiy1915@gmail.com>
2023-09-21 15:38:25 +0300

    Reffactor: remove duplicate styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <>
-      <Card key={game.id} width="300px" borderRadius={10} overflow="hidden">
+      <Card key={game.id}>
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
