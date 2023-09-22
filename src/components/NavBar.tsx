004b775fc2a5965ae267c913fd68510247933720
Olexandr <nadtochiy1915@gmail.com>
2023-09-22 22:24:33 +0300

    Implemented searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,15 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColotModeSwitch from "./ColotModeSwitch";
 import SearchInput from "./SearchInput";
+interface Props {
+  onSearch: (searchText: string) => void;
+}
 
-const NavBar = () => {
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColotModeSwitch />
     </HStack>
   );
